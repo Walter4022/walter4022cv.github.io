f4e5945b4275db78187f63e40903affe89272133
Walter <102666662+Walter4022@users.noreply.github.com>
2023-03-08 20:50:27 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var menu_visible = false;
+let menu = Document.getElememById("nav");
+function mostrarOcultarMenu() {
+  if (menu_visible == false) {
+    menu.style.display = "block";
+    menu_visible = true;
+  } else {
+    menu.style.display = "none";
+    menu_visible = false;
+  }
+}
+// ocultar menu una vez que selecciono una opcion //
+let links = document.querySelectorAll("nav a");
+for (var x = 0; x < links.length; x++) {
+  links[x].onclick = function () {
+    menu.style.display = "none";
+    menu_visible = false;
+  };
+}
